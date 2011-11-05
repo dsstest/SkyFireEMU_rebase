8b478c3ebfcecbf0c5c000e0b7292f4e00263b29
Bootz <Stage6Dev@EMPulseGaming.com>
2011-11-05 16:33:22 -0500

    Implementation of tc branch (Wintergrasp_clean-up)
    still 46 errors to clear out... but cant do anything tired so ...pushing and finishing later
    
    Signed-off-by: Bootz <Stage6Dev@EMPulseGaming.com>

--- src/server/game/AI/CreatureAISelector.cpp
+++ src/server/game/AI/CreatureAISelector.cpp
@@ -134,7 +134,11 @@ namespace FactorySelector
         GameObjectAIRegistry& ai_registry(*GameObjectAIRepository::instance());
 
         ai_factory = ai_registry.GetRegistryItem(go->GetAIName());
-
+                
+        //scriptname in db
+        if (!ai_factory)
+            if (GameObjectAI* scriptedAI = sScriptMgr->GetGameObjectAI(go))
+    
         //future goAI types go here
 
         std::string ainame = (ai_factory == NULL || go->GetScriptId()) ? "NullGameObjectAI" : ai_factory->key();
